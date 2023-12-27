d9a763281bc459e83ebd78cd44bece2973f162ec
faisalsiddique4400 <faisalsiddique10886@gmail.com>
2023-12-27 19:38:20 +0500

    add alert msg at login

--- src/pages/moviesList/index.tsx
+++ src/pages/moviesList/index.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 import Utils from '../../redux/utils';
 import Attributes from '../../redux/middleware/attributes';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { Alert } from 'antd';
 
 
 const { Meta } = Card;
@@ -23,8 +24,6 @@ const MoviesList = () => {
     removeCurrentUser();
     navigate('/auth');
   };
-
-
 
   const itemRender: PaginationProps['itemRender'] = (_, type, originalElement) => {
     if (type === 'prev') {
@@ -53,7 +52,7 @@ const MoviesList = () => {
     fetchMovies();
   }, [movies]);
 
-
+  console.log(movies);
 
   const { t } = useTranslation('translation', {
     keyPrefix: 'Movies List',
@@ -66,161 +65,163 @@ const MoviesList = () => {
     padding: '10px'
   };
   return (
-    <>
-      <div className='movies-container'>
-        <div className='custom-container'>
-          <div className='heading-movies'>
-            <div className='add-movie'>
-              <h2>{t("title")}</h2>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <g clip-path="url(#clip0_3_576)">
-                  <path d="M13 7H11V11H7V13H11V17H13V13H17V11H13V7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="white" />
-                </g>
-                <defs>
-                  <clipPath id="clip0_3_576">
-                    <rect width="24" height="24" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </div>
+    <div className='movies-container'>
+      <div className='custom-container'>
+        {successMessage && (
+          <Alert message={successMessage} type="success" closable />
+        )}
+        {/* Display success message using Ant Design Alert */}
+        <div className='heading-movies'>
+          <div className='add-movie'>
+            <h2>{t("title")}</h2>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <g clip-path="url(#clip0_3_576)">
+                <path d="M13 7H11V11H7V13H11V17H13V13H17V11H13V7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="white" />
+              </g>
+              <defs>
+                <clipPath id="clip0_3_576">
+                  <rect width="24" height="24" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </div>
 
-            <div className='logout-section' onClick={handleLogout}>
-              <p >{t("logout")}</p>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <g clip-path="url(#clip0_6_82)">
-                  <path d="M17 8L15.59 9.41L17.17 11H9V13H17.17L15.59 14.58L17 16L21 12L17 8ZM5 5H12V3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H12V19H5V5Z" fill="white" />
-                </g>
-                <defs>
-                  <clipPath id="clip0_6_82">
-                    <rect width="24" height="24" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </div>
-          </div>
-          <div className='movies-card'>
-            <Row gutter={[16, 16]}>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test-new.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test-1.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test-new.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test-1.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test-new.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test-1.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={6}>
-                <Card
-                  hoverable
-                  style={{ ...cardStyle }}
-                  cover={<img alt="example" src="/images/test-new.svg" />}
-                >
-                  <Meta title="Movie" description="2021" className='card-content' />
-                </Card>
-              </Col>
-            </Row>
-          </div>
-          <div className='pagination-section'>
-            <Pagination total={2}
-              showSizeChanger={false}
-              showQuickJumper={false}
-              pageSize={1}
-              itemRender={itemRender}
-            />
+          <div className='logout-section' onClick={handleLogout}>
+            <p >{t("logout")}</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <g clip-path="url(#clip0_6_82)">
+                <path d="M17 8L15.59 9.41L17.17 11H9V13H17.17L15.59 14.58L17 16L21 12L17 8ZM5 5H12V3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H12V19H5V5Z" fill="white" />
+              </g>
+              <defs>
+                <clipPath id="clip0_6_82">
+                  <rect width="24" height="24" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
           </div>
         </div>
+        <div className='movies-card'>
+          <Row gutter={[16, 16]}>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test-new.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test-1.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test-new.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test-1.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test-new.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test-1.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <Card
+                hoverable
+                style={{ ...cardStyle }}
+                cover={<img alt="example" src="/images/test-new.svg" />}
+              >
+                <Meta title="Movie" description="2021" className='card-content' />
+              </Card>
+            </Col>
+          </Row>
+        </div>
+        <div className='pagination-section'>
+          <Pagination total={2}
+            showSizeChanger={false}
+            showQuickJumper={false}
+            pageSize={1}
+            itemRender={itemRender}
+          />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
