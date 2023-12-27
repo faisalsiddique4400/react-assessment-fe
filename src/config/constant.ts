4bf7759bf5fda4742933cf2ea98207c448c25118
tahakhanabdalli <tahakhanabdalli@gmail.com>
2023-12-27 11:37:45 +0500

    Backend Url changed

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,2 +1,2 @@
-export const baseURL = 'https://admin.sadaynaal.com';
+export const baseURL = 'https://963d-2400-adc5-412-2c00-c8de-1d47-e531-ebdf.ngrok-free.app';
 // export const baseURL = 'https://admin.chefadmin.com';
