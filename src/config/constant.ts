308f94ceec6f1e841282ab4e43c922d6b4e73e7a
tahakhanabdalli <tahakhanabdalli@gmail.com>
2023-12-27 18:36:58 +0500

    Change base url

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,2 +1,2 @@
-export const baseURL = 'https://9fa3-2400-adc5-412-2c00-88fa-5f0a-1985-963d.ngrok-free.app';
+export const baseURL = 'http://34.70.213.6/api';
 // export const baseURL = 'https://admin.chefadmin.com';
